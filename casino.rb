96d44cbd6e5d961dc571aa4f84c9ad0c91c5efb4
Evan Wilson <wilsonevan100@gmail.com>
2019-02-15 15:07:03 -0700

    Initial Commit

---
+++ casino.rb
@@ -0,0 +1,91 @@
+require "pry"
+require "colorize"
+
+
+class Casino
+  def initialize
+    
+    # Welcome
+    # Get Input - user name & wallet amount
+    get_user_input
+
+    # Display Menu and receive input:
+    # List out the individual games (each is a class)
+    display_main_menu
+
+    # Requirements for every game:
+    # - Don't let the user keep playing if they are out of money
+    # - Must return the current wallet amount (int)
+    # - Minimum bet is $1
+    # - Constantly display wallet amount, each round of the game
+  
+  
+
+
+    # Print out final wallet amount after each game 
+    # Allow user to play a new game
+    # Allow user to add more money into the wallet
+    # allow user to exit the application
+  end
+
+  def get_user_input
+    puts "Welcome to Money City!"
+    puts "What is your name?"
+    @name = gets.strip
+    puts "How much money do you want to add to your wallet today?"
+    @wallet = gets.to_i
+    puts "Great, let's get started!"
+    sleep(1)
+  end
+
+  def display_main_menu
+    puts "-----------------------"
+    puts "Main Menu:"
+    puts "1)Roulette"
+    puts "2)Black Jack"
+    puts "3)Slots"
+    puts "4)Dice"
+    puts "5)Horse Races"
+    choice = gets.to_i
+    case choice
+    when 1 # Roulette
+      @wallet = Roullette.new(@wallet)
+
+    when 2 # Black Jack
+      @wallet = BlackJack.new(@wallet)
+    when 3 # Slots
+      @wallet = Slots.new(@wallet)
+    when 4 # Dice
+      @wallet = Dice.new(@wallet)
+    when 5 # Horse Races
+      @wallet = HorseRaces.new(@wallet)
+    end
+  end
+
+  def repeat_menu
+    #TBD
+  end
+
+
+end
+
+# class Roullette
+#   # attr_accessor :wallet
+  
+#   def initialize(wallet_of_this_game)
+#     @wallet_of_this_game = wallet_of_this_game
+#   end
+
+#   def spin_the_wheel
+
+#     #example game functionality
+
+#     cash_out
+#   end
+
+#   def cash_out
+#     return @wallet_of_this_game = @wallet_of_this_game + 5
+#   end
+# end
+
+Casino.new
