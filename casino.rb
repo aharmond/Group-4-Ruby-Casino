7ff2cc01e2bfaf7e095d9e0716d48a4ec92abd28
Thanh Bui <ttthanhbui@gmail.com>
2019-02-16 10:41:29 -0700

    slots

--- casino.rb
+++ casino.rb
@@ -46,6 +46,7 @@ class Casino
     puts "3)Slots"
     puts "4)Dice"
     puts "5)Horse Races"
+    puts "6) Exit"
     choice = gets.to_i
     case choice
     when 1 # Roulette
@@ -54,16 +55,87 @@ class Casino
     when 2 # Black Jack
       @wallet = BlackJack.new(@wallet)
     when 3 # Slots
+      slots_menu
       @wallet = Slots.new(@wallet)
     when 4 # Dice
       @wallet = Dice.new(@wallet)
     when 5 # Horse Races
       @wallet = HorseRaces.new(@wallet)
+    when 6
+      exit
+    else
+      puts "Invalid choice. Try again."
+      sleep(1)
+      display_main_menu
     end
   end
 
   def repeat_menu
     #TBD
+  end
+
+  def slots_menu
+    puts "------------------------------------"
+    puts "Welcome to Lucky Money Slot Machine!"
+    puts "1) Start a new game"
+    puts "2) Access to wallet"
+    puts "3) Exit to Main Menu"
+    choice = gets.to_i
+    case choice
+    when 1
+      start_game
+    when 2
+      access_wallet
+    when 3
+      display_main_menu
+    end
+  end
+
+  def start_game
+    puts "Place your bet: "
+    @slots_bet = gets.to_i
+    if @slots_bet > @wallet
+      puts "You only have $#{@wallet}."
+      puts "1) Place lower bet."
+      puts "2) Access to wallet."
+      puts "3) Exit to Main Menu."
+      choice = gets.to_i
+      case choice
+      when 1
+        start_game
+      when 2
+        access_wallet
+      when 3
+        display_main_menu
+      else
+        puts "Invalid choice. Try again"
+        slots_menu
+      end
+    else
+      puts "Spinning..."
+      sleep(3)
+      result = Random.new
+      puts result = rand(1..30).to_i
+      sleep(1)
+      if result == 19
+        win
+      else
+        lose
+      end
+    end
+    sleep(2)
+    slots_menu
+  end
+
+  def win
+    puts "Congrats! You win $#{@slots_bet}!"
+    puts "Your wallet now has $#{@wallet + @slots_bet}."
+  end
+
+  def lose
+    puts "Sorry! You lost $#{@slots_bet}"
+    puts "Your wallet now has $#{@wallet - @slots_bet}."
+    puts "Better luck next time!"
   end
 
 
@@ -88,4 +160,6 @@ end
 #   end
 # end
 
-Casino.new
+casino = Casino.new
+casino
+
